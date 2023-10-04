cb6b62feda0ab390e696f0baf411db9edb883df5
AureDev62 <audioslave29@gmail.com>
2023-10-04 16:45:03 +0200

    exo2

--- src/main/java/org/example/Exo2/Exo2.java
+++ src/main/java/org/example/Exo2/Exo2.java
@@ -4,5 +4,16 @@ package org.example.Exo2;
 import java.util.Scanner;
 
 public class Exo2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez entrer une chaîne de caractère");
+        String str = scanner.nextLine();
 
+        try {
+            int nbre = Integer.parseInt(str);
+            System.out.println("Le nombre est :" +nbre);
+        }catch (NumberFormatException e){
+            System.out.println("Ceci n'est pas une chaîne de caractère");
+        }
+    }
 }
